a4796834297c588c93c3619f350061587606381c
Sara Farias <fariassara012@gmail.com>
2026-03-15 18:10:22 -0300

    Update Categoria.php

--- src/app/Models/Categoria.php
+++ src/app/Models/Categoria.php
@@ -6,5 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Categoria extends Model
 {
-    //
+    protected $fillable = ['nome', 'horas'];
+
+    public function chamados()
+    {
+        return $this->hasMany(Chamado::class);
+    }
 }
